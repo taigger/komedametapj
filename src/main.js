1203193e6055fdb1fdf5e167e707b1d0add7c49c
taigger <hidenavi92soh1390@gmail.com>
2021-11-19 20:06:05 +0900

    lunar-eclipse

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './main.css';
 
 
+
+
 function Main(){
+
+    const [ImageMouseOver,setImageMouseOver] = useState(false)
 
     return(
         <div className="main">
             <div className="posts">
                 <div>   
-                    <img src="https://drive.google.com/uc?id=1B2QjGQYfwW33rfOUpaWny7vQotD7iCxl"></img>
+                    <img 
+                    src="https://drive.google.com/uc?id=1B2QjGQYfwW33rfOUpaWny7vQotD7iCxl"
+                    onMouseEnter={()=> setImageMouseOver(true)}
+                    onMouseLeave={()=> setImageMouseOver(false)}
+                    />
+                    { ImageMouseOver && (
+                        <div>
+                         "it is fine"
+                        </div>
+                        )}
                 </div>
             </div>
         </div>
